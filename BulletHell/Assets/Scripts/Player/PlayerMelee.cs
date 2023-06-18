0305ce4bca1e33f65c816d0a47be9f5745471c61
Viwka <vwko111@gmail.com>
2023-06-18 19:58:45 +0300

    StartedWork WIth Anims

---
+++ BulletHell/Assets/Scripts/Player/PlayerMelee.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMelee : MonoBehaviour
+{
+    public int dmg = 3;
+    float firerate = 0.5f;
+    float canfire = 0.5f;
+    public Collider2D hitbox;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time > canfire)
+        {
+            Swing();
+            canfire = Time.time + firerate;
+        }
+
+        void Swing()
+        {
+            hitbox.
+        }
+    }
+}
